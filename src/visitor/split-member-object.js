320cb7bb5395ec9cf78923891d4af2f73a6c0ad4
echo094 <20028238+echo094@users.noreply.github.com>
2024-07-07 01:52:32 +0800

    visitor(split-member-object): add new visitor
    Signed-off-by: echo094 <20028238+echo094@users.noreply.github.com>

---
+++ src/visitor/split-member-object.js
@@ -0,0 +1,38 @@
+function splitMemberObject(path) {
+  const object = path.get('object')
+  if (!object.isAssignmentExpression()) {
+    return
+  }
+  let insertPath = path
+  while (!insertPath?.listKey) {
+    if (insertPath.parentPath.isAssignmentExpression()) {
+      insertPath = insertPath.parentPath
+      continue
+    }
+    if (insertPath.parentPath.isExpressionStatement()) {
+      insertPath = insertPath.parentPath
+      continue
+    }
+    return
+  }
+  insertPath.insertBefore(object.node)
+  object.replaceWith(object.node.left)
+  insertPath.scope.crawl()
+}
+
+/**
+ * Split assignment operation in member object
+ *
+ * From:
+ * ```javascript
+ * (a = {})['b'] = c;
+ * ```
+ * To:
+ * ```javascript
+ * a = {}
+ * a['b'] = c;
+ * ```
+ */
+module.exports = {
+  MemberExpression: splitMemberObject,
+}
