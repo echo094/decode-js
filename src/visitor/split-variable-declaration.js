8950b81fb3484128fb4c83561e27873104804940
echo094 <20028238+echo094@users.noreply.github.com>
2024-07-06 16:48:18 +0800

    visitor(split-variable-declaration): A a basic visitor
    Signed-off-by: echo094 <20028238+echo094@users.noreply.github.com>

---
+++ src/visitor/split-variable-declaration.js
@@ -0,0 +1,30 @@
+const t = require('@babel/types')
+
+function splitVariableDeclaration(path) {
+  // The scope of a for statement is its body
+  if (path.parentPath.isFor()) {
+    return
+  }
+  // The container must be an array
+  if (!path.listKey) {
+    return
+  }
+  const kind = path.node.kind
+  const list = path.node.declarations
+  if (list.length == 1) {
+    return
+  }
+  for (let item of list) {
+    path.insertBefore(t.variableDeclaration(kind, [item]))
+  }
+  path.remove()
+}
+
+/**
+ * Split the VariableDeclaration if it has more than one VariableDeclarator
+ *
+ * This operation will only be performed when its container is an array
+ */
+module.exports = {
+  VariableDeclaration: splitVariableDeclaration,
+}
