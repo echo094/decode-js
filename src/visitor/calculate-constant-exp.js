f4e2a8f3d0c5305510240d1b6974676c1794ad3f
echo094 <20028238+echo094@users.noreply.github.com>
2024-07-07 00:39:17 +0800

    visitor(calculate-constant-exp): fix bug when calculating strings
    Signed-off-by: echo094 <20028238+echo094@users.noreply.github.com>

--- src/visitor/calculate-constant-exp.js
+++ src/visitor/calculate-constant-exp.js
@@ -16,6 +16,13 @@ function calculateBinaryExpression(path) {
   try {
     const ret = eval(code)
     path.replaceWithSourceString(ret)
+    // The strings cannot use replaceWithSourceString
+    // For example, string "ab" will be parsed as identifier ab
+    if (typeof ret === 'string') {
+      path.replaceWith(t.stringLiteral(ret))
+    } else {
+      path.replaceWithSourceString(ret)
+    }
   } catch {
     //
   }
