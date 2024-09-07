65b1879636f0237c2d36f7c0289beb356e24a6e8
echo094 <20028238+echo094@users.noreply.github.com>
2024-09-07 15:18:25 +0800

    Stack functionLengthName

--- src/plugin/jsconfuser.js
+++ src/plugin/jsconfuser.js
@@ -10,6 +10,10 @@ function virtualGlobalEval(jsStr) {
   return globalContext.evalSync(String(jsStr))
 }
 
+const collect_id = {
+  arrowFunc: null,
+}
+
 function deAntiToolingCheckFunc(path) {
   if (path.node.params.length) {
     return false
@@ -107,12 +111,25 @@ function checkArrowWrap(path) {
   }
 }
 
+/**
+ * Template:
+ * ```javascript
+ * function arrowFunctionName (arrowFn, functionLength = 0){
+ *   var functionObject = function(){ return arrowFn(...arguments) };
+ *   return Object.defineProperty(functionObject, "length", {
+ *     "value": functionLength,
+ *     "configurable": true
+ *   });
+ * }
+ * ```
+ */
 const deMinifyArrow = {
   Identifier(path) {
     let obj = checkArrowWrap(path)
     if (!obj) {
       return
     }
+    collect_id.arrowFunc = obj.name
     console.log(`Find arrowFunctionName: ${obj.name}`)
     let binding = obj.path.parentPath.scope.bindings[obj.name]
     for (const ref of binding.referencePaths) {
@@ -125,7 +142,71 @@ const deMinifyArrow = {
     }
     binding.scope.crawl()
     binding = obj.path.parentPath.scope.bindings[obj.name]
-    if (!binding.references) [obj.path.remove()]
+    if (!binding.references) {
+      obj.path.remove()
+    }
+  },
+}
+
+function checkFuncLen(path) {
+  if (path.node?.name !== 'configurable' || path.key !== 'key') {
+    return null
+  }
+  const prop = path.parentPath
+  if (!prop.isObjectProperty() || prop.key !== 1) {
+    return null
+  }
+  const obj = prop.parentPath
+  if (obj.node.properties.length !== 2) {
+    return null
+  }
+  if (obj.node.properties[0]?.key?.name !== 'value') {
+    return null
+  }
+  if (obj.listKey !== 'arguments' || obj.key !== 2) {
+    return null
+  }
+  const func_name = obj.container[0]?.name
+  const warp = obj.getFunctionParent()
+  if (warp.node.params?.[0]?.name !== func_name) {
+    return null
+  }
+  const func_len_name = warp.node?.id?.name
+  if (!func_len_name || func_len_name === collect_id.arrowFunc) {
+    return null
+  }
+  return {
+    name: func_len_name,
+    path: warp,
+  }
+}
+
+const deStackFuncLen = {
+  Identifier(path) {
+    let obj = checkFuncLen(path)
+    if (!obj) {
+      return
+    }
+    console.log(`Find functionLengthName: ${obj.name}`)
+    let binding = obj.path.parentPath.scope.bindings[obj.name]
+    for (const ref of binding.referencePaths) {
+      if (ref.key !== 'callee') {
+        console.warn(`Unexpected ref of functionLengthName: ${obj.name}`)
+        continue
+      }
+      const repl_path = ref.parentPath
+      const arg = repl_path.node.arguments[0]
+      if (t.isIdentifier(arg)) {
+        repl_path.remove()
+      } else {
+        repl_path.replaceWith(arg)
+      }
+    }
+    binding.scope.crawl()
+    binding = obj.path.parentPath.scope.bindings[obj.name]
+    if (!binding.references) {
+      obj.path.remove()
+    }
   },
 }
 
@@ -141,6 +222,8 @@ module.exports = function (code) {
   traverse(ast, deAntiTooling)
   // Minify
   traverse(ast, deMinifyArrow)
+  // Stack
+  traverse(ast, deStackFuncLen)
   code = generator(ast, {
     comments: false,
     jsescOption: { minimal: true },
